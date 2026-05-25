77947134d9d0aa7e98c48435b39bd32f5c07a3b5
gcharvini <gcharvini1310@gmail.com>
2026-05-25 22:13:07 +0530

    Adding Springboot project created by a springboot initialzr

---
+++ src/main/java/com/Charvini/WebCProject/WebCProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Charvini.WebCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCProjectApplication.class, args);
+	}
+
+}
