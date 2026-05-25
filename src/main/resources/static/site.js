59d194998b0c82c6727fb251265c449ad540736b
gcharvini <gcharvini1310@gmail.com>
2026-05-26 00:09:34 +0530

    error1solving

--- src/main/resources/static/site.js
+++ src/main/resources/static/site.js
@@ -8,11 +8,11 @@ var users = [
 var currentUserIndex = 0;
 
 function toggleCard(){
-    currentUserIndex =(currentUserIndex + 1) % 2;
+   currentUserIndex = (currentUserIndex + 1) % users.length;
     var user = users[currentUserIndex];
 
     // DOM Manipulation
     document.getElementById("userImage").src = user.img;
     document.getElementById("userGender").innerHTML = user.gender;
-    document.getElementById("userName").innerHTML = user.name;
+    document.getElementById("userName").innerHTML = "<b>" + user.name + "</b>";
 }
